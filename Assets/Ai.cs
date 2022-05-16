c31aeb3a45b31c05d81ca1cd1a652c3eeb53bf60
Cwavs <16854045+Cwavs@users.noreply.github.com>
2022-05-16 15:00:27 +0100

    Update AI

--- Assets/Ai.cs
+++ Assets/Ai.cs
@@ -6,23 +6,40 @@ using UnityEngine.AI;
 public class Ai : MonoBehaviour
 {
 	private NavMeshAgent agent;
-	public Transform goal;
+	public Transform[] goals;
 	private Vector3 oldgoal;
 	
     // Start is called before the first frame update
     void Start()
     {
 		agent = GetComponent<NavMeshAgent>();
-		
+        agent.SetDestination(pickRandomGoal());
     }
 
     // Update is called once per frame
     void Update()
     {
-		if(goal.position != oldgoal)
-		{
-			agent.SetDestination(goal.position);
-			oldgoal = goal.position;
-		}
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        agent.SetDestination(pickRandomGoal());
+    }
+
+    private Vector3 pickRandomGoal()
+    {
+        int rand = Random.Range(0, goals.Length - 1);
+        Vector3 goal = goals[rand].position;
+        if(goal != oldgoal)
+        {
+            oldgoal = goal;
+            return goal;
+        }
+        else
+        {
+            return pickRandomGoal();
+        }
+        
     }
 }
